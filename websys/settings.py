baddad85248d80e747d60d588b8fc6003d504496
lauro <lauropimentel12@gmail.com>
2021-07-16 19:21:11 -0300

    env vars

--- websys/settings.py
+++ websys/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [s]
+ALLOWED_HOSTS = ['https://websysm.herokuapp.com']
 
 AUTH_USER_MODEL = 'webusers.User'
 # Application definition
