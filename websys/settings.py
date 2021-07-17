a6e3662af028d22f079ca8acf9e6486997303495
lauro <lauropimentel12@gmail.com>
2021-07-16 23:08:21 -0300

    gunicorn

--- websys/settings.py
+++ websys/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['https://websysm.herokuapp.com']
+ALLOWED_HOSTS = ['websysm.herokuapp.com', 'localhost:8000']
 
 AUTH_USER_MODEL = 'webusers.User'
 # Application definition
