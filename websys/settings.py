8e39806b742c8800e0dded5850ef9f0af76ad82f
lauro <lauropimentel12@gmail.com>
2021-07-21 22:51:41 -0300

    Ajustes na api e models

--- websys/settings.py
+++ websys/settings.py
@@ -161,7 +161,8 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://websys-frontend-euatmijsr-lauromtp.vercel.app"
 ]
 
 # Internationalization
@@ -179,8 +180,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') 
-#SECURE_SSL_REDIRECT =  True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') 
+SECURE_SSL_REDIRECT =  True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
